ae3b206d604ab4f365ba00f0c7ae20a363373770
Alan Xavier <alan_xvr@hotmail.com>
2020-02-01 19:25:37 -0400

    Create routes using express router 1. Create bootcamp routes GET, POST, PUT, DELETE using express router in routes/bootcamps.js 2. Require these routes and use them in server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
+/* Routes files */ const bootcamps = require("./routes/bootcamps");
 
 // Load Environment Variables
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Mount Routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
