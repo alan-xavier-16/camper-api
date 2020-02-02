a00c999252f4830c95ddb9359796d3053f915613
Alan Xavier <alan_xvr@hotmail.com>
2020-02-02 17:53:06 -0400

    Add duplicate fields and validation error handlers

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -5,13 +5,26 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   // Log to console for developer
-  console.log(err.stack);
   console.log(err);
 
   // Mongoose Invalid ObjectId - CastError
   if (err.name === "CastError") {
     const message = `Bootcamp not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose Duplicate Key - unique keyword in Models
+  if (err.code && err.code === 11000) {
+    const message = Object.keys(err.keyValue).map(
+      field => `Duplicate field value entered at ${field}`
+    );
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose Validation Errors - ValidationError
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res
