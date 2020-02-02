dabcb466a0a0bae8dfbebf1fb573769cace274f2
Alan Xavier <alan_xvr@hotmail.com>
2020-02-01 21:50:12 -0400

    Connect MongoDB to app using mongoose 1. Install mongoose npm package for elegant abstraction of objects for nodjs 2. Create config/db.js that has a connectDB function which uses mongoose to connect MongoDB to the application 3. Import and call this function in server.js 4. Since mongoose returns a promise, server.js was refactored to close the server for unhandled promise rejections

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+/* Load Environment Variables */ dotenv.config({ path: "./config/config.env" });
+/* Connect to database */ connectDB();
+
 /* Routes files */ const bootcamps = require("./routes/bootcamps");
-
-// Load Environment Variables
-dotenv.config({ path: "./config/config.env" });
-
 const app = express();
 
 // Development Logging Middlewares
@@ -18,6 +19,13 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+// Handle Unhandled Promise Rejections
+process.on("unhandledRejection", (error, promise) => {
+  console.log(`Error: ${error.message}`);
+  // Close Server and Exit Process
+  server.close(() => process.exit(1));
 });
