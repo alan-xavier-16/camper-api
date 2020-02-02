e6bc78ff23a7352e24d5083f6e9f7a6d8f7b65ab
Alan Xavier <alan_xvr@hotmail.com>
2020-02-01 20:17:35 -0400

    Install morgan as an HTTP request middleware 1. Install morgan as an HTTP request middleware for server.js 2. Require morgan and use the package in server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 /* Routes files */ const bootcamps = require("./routes/bootcamps");
 
 // Load Environment Variables
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Development Logging Middlewares
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // Mount Routers
 app.use("/api/v1/bootcamps", bootcamps);
