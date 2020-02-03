9b1d91b24f8a99251eeecb368338c0a7caa3478a
Alan Xavier <alan_xvr@hotmail.com>
2020-02-03 16:06:06 -0400

    Refactor the GET ALL Bottcamps route 1. Include mongoose .select() method to return the models with only the fields requested on the query 2. Include mongoose .sort() method to return the models sorted in ascending order based on the model fields request in the query

--- controllers/bootcamps.controller.js
+++ controllers/bootcamps.controller.js
@@ -8,9 +8,31 @@ const asyncHandler = require("../middleware/asyncHandler");
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
-  let queryStr = JSON.stringify(req.query);
+
+  const reqQuery = { ...req.query };
+
+  // Remove the following fields from req.query, these fields used for Select, Sorting
+  const removeFields = ["select", "sort"];
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  // Filtering using MongoDB Comparison Operators
+  let queryStr = JSON.stringify(reqQuery);
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
   query = Bootcamp.find(JSON.parse(queryStr));
+
+  // Select Fields
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+
+  // Sort Results
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
 
   const bootcamps = await query;
 
