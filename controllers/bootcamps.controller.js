2d1e36bdd7d998c71e468374a2b5258d84528e89
Alan Xavier <alan_xvr@hotmail.com>
2020-02-02 21:10:39 -0400

    Create route to find all bootcamps by distance 1. Create controller function in bootcamps.controller.js that used geocoder to return bootcamps within a certain location by using the $geoWithin MongoBD query and projection operator 2. Create a route in bootcamps.route.js to return the bootcamps with a zipcode and distance parameter

--- controllers/bootcamps.controller.js
+++ controllers/bootcamps.controller.js
@@ -1,3 +1,4 @@
+const geocoder = require("../utils/geocoder");
 const Bootcamp = require("../models/Bootcamps.models");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/asyncHandler");
@@ -80,4 +81,27 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   }
 
   res.status(200).json({ success: true, data: {} });
+});
+
+// @desc    Get bootcamps within a radius
+// @route   GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // Get latitude and longitude from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // Calculate the radius using radians, EARTH_RADIUS = 3963 m / 6378 km
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+  });
+
+  res
+    .status(200)
+    .json({ success: true, count: bootcamps.length, data: bootcamps });
 });
