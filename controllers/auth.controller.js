0fb71b74acb3ddffd93626528e46fb1559f2f9fa
Alan Xavier <alan_xvr@hotmail.com>
2020-02-06 16:05:21 -0400

    Add 'email sending' with nodemailer 1. Install nodemailer npm package 2. Create a sendEmail utility function that uses nodemailer to send an email to the user 3. Update the 'forgotPassword' route to use sendEmail to the user. If a failure occurs, the resetPasswordToken and resetPasswordExpire filed are reset to undefined for security!

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const User = require("../models/User.model");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/asyncHandler");
+const sendEmail = require("../utils/sendEmail");
 
 // @desc    Register user
 // @route   POST /api/v1/auth/register
@@ -66,7 +67,30 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
   await user.save({ validateBeforeSave: false });
 
-  res.status(200).json({ success: true, data: user });
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Password Reset Token",
+      message
+    });
+    res.status(200).json({ success: true, data: "Email sent" });
+  } catch (err) {
+    console.error(err);
+
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse(`Email could not be sent`, 500));
+  }
 });
 
 /*########################################################################################################*/
