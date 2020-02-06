e7aa50ff005cb97d7976aa5b66ec655fff6bb75e
Alan Xavier <alan_xvr@hotmail.com>
2020-02-06 14:49:33 -0400

    Create a password reset token 1. Add a method in User.model.js that generates a random token using Node's 'crypto' module and set a token expiration date. 2. Create a controller method 'forgotPassword' that uses the method created above and saves the user with the token field and expiration field updated. 3. Add a route that uses this controller method.

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,7 +2,8 @@ const express = require("express");
 /* Controller Methods for Routes */ const {
   register,
   login,
-  getMe
+  getMe,
+  forgotPassword
 } = require("../controllers/auth.controller");
 const router = express.Router();
 
@@ -11,5 +12,6 @@ const { protect } = require("../middleware/auth.middlewares");
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/me").get(protect, getMe);
+router.route("/forgotpassword").post(forgotPassword);
 
 module.exports = router;
