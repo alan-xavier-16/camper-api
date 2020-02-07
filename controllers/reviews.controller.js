412e3e1810f3bc2a1b065d90a92353181ad90caf
Alan Xavier <alan_xvr@hotmail.com>
2020-02-07 14:59:44 -0400

    Add Update and Delete review routes

--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -59,3 +59,56 @@ exports.addReview = asyncHandler(async (req, res, next) => {
 
   res.status(201).json({ success: true, data: review });
 });
+
+// @desc    Update a review
+// @route   PUT /api/v1/reviews/:id
+// @access  Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  let review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(
+        `No review with the id of ${req.params.bootcampId}`,
+        404
+      )
+    );
+  }
+
+  // Check review ownership
+  if (review.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(new ErrorResponse(`Not authorized to update review`, 401));
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(201).json({ success: true, data: review });
+});
+
+// @desc    Delete a review
+// @route   DELETE /api/v1/reviews/:id
+// @access  Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(
+        `No review with the id of ${req.params.bootcampId}`,
+        404
+      )
+    );
+  }
+
+  // Check review ownership
+  if (review.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(new ErrorResponse(`Not authorized to update review`, 401));
+  }
+
+  await review.remove();
+
+  res.status(201).json({ success: true, data: {} });
+});
--- routes/reviews.route.js
+++ routes/reviews.route.js
@@ -2,7 +2,9 @@ const express = require("express");
 const {
   getReviews,
   getReview,
-  addReview
+  addReview,
+  updateReview,
+  deleteReview
 } = require("../controllers/reviews.controller");
 const router = express.Router({ mergeParams: true });
 
@@ -18,6 +20,10 @@ router
   )
   .post(protect, authorize("user", "admin"), addReview);
 
-router.route("/:id").get(getReview);
+router
+  .route("/:id")
+  .get(getReview)
+  .put(protect, authorize("user", "admin"), updateReview)
+  .delete(protect, authorize("user", "admin"), deleteReview);
 
 module.exports = router;
